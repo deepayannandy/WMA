f5ec97c95695b1083b36e78853cf9e6c05dae180
deepnandy <deepayan.622@gmail.com>
2020-05-10 04:39:38 +0530

    5am

--- app/src/main/java/com/dnyindia/wma/MainActivity.kt
+++ app/src/main/java/com/dnyindia/wma/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.dnyindia.wma
+import android.content.Context
 import android.content.Intent
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SimpleCursorAdapter
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,17 +17,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var dBase: SQLiteDatabase = openOrCreateDatabase(
+            "history",
+            Context.MODE_PRIVATE, null)
+        dBase.execSQL("create table if not exists log(name varchar(50),contact varchar(50),address varchar(50),amount varchar(50))")
 
 
         scan.setOnClickListener{
 
             val intent=Intent(this,scanneActivity::class.java)
-            //intent.putExtra("user_data","Deepayan Nandy/U1/7384213622/Budbud")
             startActivity(intent)
 
         }
         Logs.setOnClickListener{
-            Toast.makeText(this,"Logs Clicked!",Toast.LENGTH_SHORT).show()
+
+            var c:Cursor= dBase.query("log", null,null,null,null,null,null)
+            Toast.makeText(this, c.toString(), Toast.LENGTH_LONG).show()
+            /*var cAdapter = SimpleCursorAdapter(
+                this,R.layout.row, c, arrayOf("name","contact","address","amount"), intArrayOf(R.id.title_name,R.id.text_contact,R.id.text_address,R.id.text_amount),0)
+
+            list_data.adapter = cAdapter*/
         }
 
     }
--- app/src/main/java/com/dnyindia/wma/calpayActivity.kt
+++ app/src/main/java/com/dnyindia/wma/calpayActivity.kt
@@ -1,7 +1,10 @@
 package com.dnyindia.wma
 
 
+import android.content.ContentValues
+import android.content.Context
 import android.content.Intent
+import android.database.sqlite.SQLiteDatabase
 import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
@@ -13,6 +16,13 @@ class calpayActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calpay)
+
+        var dBase: SQLiteDatabase = openOrCreateDatabase(
+            "history",
+            Context.MODE_PRIVATE, null)
+        dBase.execSQL("create table if not exists log(name varchar(50),contact varchar(50),address varchar(50),amount varchar(50))")
+
+
 
         var amount =0.0
         val builde :Bundle?=intent.extras
@@ -33,21 +43,29 @@ class calpayActivity : AppCompatActivity(){
             total.text=t.toString()+" Rs. Only"
             amount= t.toDouble()
         }
-        pay.setOnClickListener() {
-            Toast.makeText(this,data[2]+" Rs. "+amount.toString() , Toast.LENGTH_LONG).show()
-            val i = packageManager.getLaunchIntentForPackage("net.one97.paytm")
-            if (i != null) {
-                startActivity(i)
-            }
+        pay.setOnClickListener {
 
-            else {
-                var i = Intent()
-                i.action = Intent.ACTION_VIEW
-                i.data = Uri.parse("https://play.google.com/store/apps/details?id=net.one97.paytm&hl=en")
-                startActivity(i)
+            var cv = ContentValues( )
+            cv.put("name",data[0])
+            cv.put("Contact",data[2])
+            cv.put("address",data[3])
+            cv.put("amount",amount.toString())
+            var status:Long = dBase.insert("log",
+                null, cv)
+            if(status != -1L){
+                Toast.makeText(this, "Record Inserted Successfully", Toast.LENGTH_LONG).show()
+            }else{
+                Toast.makeText(this, "Record insertion  is failed..", Toast.LENGTH_LONG).show()
             }
+            dBase.close()
+            val intent=Intent(this,MainActivity::class.java)
+            startActivity(intent)
 
         }
+
+
+
+
 
     }
 }
